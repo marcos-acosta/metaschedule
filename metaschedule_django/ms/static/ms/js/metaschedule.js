81fde60baf761e44b7a04b1ba1a96a3283edb01b
Marcos Acosta <macosta1625@gmail.com>
2020-06-08 11:29:52 -0400

    Basic add/drop courses now complete

--- metaschedule_django/ms/static/ms/js/metaschedule.js
+++ metaschedule_django/ms/static/ms/js/metaschedule.js
@@ -32,15 +32,26 @@ $(document).ready(function(){
     });
     /* Type in search bar */
     $("#courseSearch").keyup(function(){
-        // alert($(this).val());
         let query = $("#courseSearch").val().toLowerCase();
-        let results = searchStrings(query, keys);
-        let cards = constructCards(results, 10);
-        $("#searchResults").replaceWith(cards);
+        if (query == '') {
+            $("#searchResults").replaceWith('<div id="searchResults"></div>');
+        } else {
+            let results = searchStrings(query, keys);
+            let cards = constructCards(results, 10);
+            $("#searchResults").replaceWith(cards);
+        }
     });
     /* Add a course to your cart */
-    $("#resultsContainer").on("click", ".close", function(){
-        console.log($(this).attr("data-id"));
+    $("#resultsContainer").on("click", "#addCourseButton", function(){
+        let code = $(this).attr("data-id");
+        selectedCourses.push(code);
+        updateCourseList();
+    })
+    /* Remove a course from your cart */
+    $("#rightSide").on("click", "#removeCourseButton", function(){
+        let code = $(this).attr("data-id");
+        removeItemOnce(selectedCourses, code);
+        updateCourseList();
     })
 });
 
@@ -64,15 +75,58 @@ function searchStrings (query, arr) {
 
 /* Builds cards to select from list of results */
 function constructCards(results, cap) {
-    results = results.slice(0, Math.min(cap, results.length));
+    let coursesAdded = 0;
     let cardsHtml = '<div id="searchResults">';
     for (let i = 0; i < results.length; i++) {
-        cardsHtml +=    '<div class="card"><div class="card-body">' + results[i] + ' | ' + 
+        if (selectedCourses.includes(results[i])) {
+            continue;
+        }
+        cardsHtml +=    '<div class="card" id="' + results[i] + '"><div class="card-body">' + results[i] + ' | ' + 
                         course_data[results[i]]['courseName'] + '<span class="text-muted ml-2">' + 
                         course_data[results[i]]['courseCredits'] + ' credits</span>' + 
-                        '<button type="button" class="close float-right" data-id="' + results[i] + '">' + 
+                        '<button type="button" class="close float-right" id="addCourseButton" data-id="' + results[i] + '">' + 
                         '<span aria-hidden="true">+</span></button></div></div>';
+        coursesAdded++;
+        if (coursesAdded == cap) {
+            break;
+        }
     }
     cardsHtml += '</div>';
     return cardsHtml;
+}
+
+function constructSelectedCards() {
+    let cardsHtml = '<div id="courseList">';
+    for (let i = 0; i < selectedCourses.length; i++) {
+        cardsHtml +=    '<div class="card" id="' + selectedCourses[i] + '"><div class="card-body">' + selectedCourses[i] + ' | ' + 
+                        course_data[selectedCourses[i]]['courseName'] + '<span class="text-muted ml-2">' + 
+                        course_data[selectedCourses[i]]['courseCredits'] + ' credits</span>' + 
+                        '<button type="button" class="close float-right" id="removeCourseButton" data-id="' + selectedCourses[i] + '">' + 
+                        '<span aria-hidden="true">&times;</span></button></div></div>';
+    }
+    cardsHtml += '</div>';
+    return cardsHtml;
+}
+
+function updateCourseList() {
+    $("#courseSearch").trigger("keyup");
+    let cards = constructSelectedCards();
+    $("#courseList").replaceWith(cards);
+    $("#creditCount").replaceWith('<span id="creditCount">' + getCreditTotal() + '</span>');
+}
+
+function removeItemOnce(arr, value) { 
+    let index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
+
+function getCreditTotal() {
+    let count = 0.0;
+    for (let i = 0; i < selectedCourses.length; i++) {
+        count += parseFloat(course_data[selectedCourses[i]]['courseCredits'])
+    }
+    return count;
 }
