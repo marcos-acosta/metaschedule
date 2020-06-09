64d1dfb68b0d68d4bc7fa4e95c255c6bf548fe76
Marcos Acosta <macosta1625@gmail.com>
2020-06-08 22:47:24 -0400

    Colors\!

--- metaschedule_django/ms/static/ms/js/metaschedule.js
+++ metaschedule_django/ms/static/ms/js/metaschedule.js
@@ -2,6 +2,7 @@
 var course_data;
 var selectedCourses = [];
 var keys;
+var stumps;
 
 /* JQUERY */
 $(document).ready(function(){
@@ -9,6 +10,7 @@ $(document).ready(function(){
     $.getJSON('https://hyperschedule.herokuapp.com/api/v3/courses?school=hmc', function(all_data) {
         course_data = all_data['data']['courses'];
         keys = Object.keys(course_data);
+        instantiateStumps();
         console.log('Got course data');
     });
     /* Switch between tabs */
@@ -19,12 +21,6 @@ $(document).ready(function(){
     $("#schedulesButton").change(function() {
         $("#searchContainer").hide();
         $("#scheduleContainer").show();
-    });
-    /* Search button */
-    $("#search").click(function(){
-        // alert(course_data['data']['courses']['AFRI 010A AF-01']['courseCredits'])
-        const stump = new CodeStump('AFRI 010A AF', [1, 2, 3, 4]);
-        alert(stump.codeStump);
     });
     /* Test button */
     $("#testButton").click(function(){
@@ -81,11 +77,11 @@ function constructCards(results, cap) {
         if (selectedCourses.includes(results[i])) {
             continue;
         }
-        cardsHtml +=    '<div class="card" id="' + results[i] + '"><div class="card-body">' + results[i] + ' | ' + 
-                        course_data[results[i]]['courseName'] + '<span class="text-muted ml-2">' + 
-                        course_data[results[i]]['courseCredits'] + ' credits</span>' + 
+        cardsHtml +=    '<div class="card '+ getColor(results[i]) +' mb-1" id="' + results[i] + '" style="border-color: rgba(255, 0, 0, 0);">' + 
+                        '<div class="card-body">' + results[i] + ' | ' + course_data[results[i]]['courseName'] + ' | ' + 
+                        course_data[results[i]]['courseCredits'] + ' credits' + 
                         '<button type="button" class="close float-right" id="addCourseButton" data-id="' + results[i] + '">' + 
-                        '<span aria-hidden="true">+</span></button></div></div>';
+                        '<span aria-hidden="true" style="color: white;">+</span></button></div></div>';
         coursesAdded++;
         if (coursesAdded == cap) {
             break;
@@ -98,7 +94,7 @@ function constructCards(results, cap) {
 function constructSelectedCards() {
     let cardsHtml = '<div id="courseList">';
     for (let i = 0; i < selectedCourses.length; i++) {
-        cardsHtml +=    '<div class="card" id="' + selectedCourses[i] + '"><div class="card-body">' + selectedCourses[i] + ' | ' + 
+        cardsHtml +=    '<div class="card mb-1" id="' + selectedCourses[i] + '"><div class="card-body">' + selectedCourses[i] + ' | ' + 
                         course_data[selectedCourses[i]]['courseName'] + '<span class="text-muted ml-2">' + 
                         course_data[selectedCourses[i]]['courseCredits'] + ' credits</span>' + 
                         '<button type="button" class="close float-right" id="removeCourseButton" data-id="' + selectedCourses[i] + '">' + 
@@ -129,4 +125,44 @@ function getCreditTotal() {
         count += parseFloat(course_data[selectedCourses[i]]['courseCredits'])
     }
     return count;
+}
+
+function getColor(code) {
+    let len = stumps.length;
+    let index = stumps.indexOf(stumpify(code));
+    if (index < len/10) {
+        return "redCard";
+    } else if (index < 2 * (len/10)) {
+        return "greenCard";
+    } else if (index < 3 * (len/10)) {
+        return "yellowCard";
+    } else if (index < 4 * (len/10)) {
+        return "blueCard";
+    } else if (index < 5 * (len/10)) {
+        return "orangeCard";
+    } else if (index < 6 * (len/10)) {
+        return "purpleCard";
+    } else if (index < 7 * (len/10)) {
+        return "mintCard";
+    } else if (index < 8 * (len/10)) {
+        return "pinkCard";
+    } else if (index < 9 * (len/10)) {
+        return "aquaCard";
+    } else {
+        return "steelCard";
+    }
+}
+
+function instantiateStumps() {
+    stumps = new Set();
+    for (let i = 0; i < keys.length; i++) {
+        stumps.add(stumpify(keys[i]));
+    }
+    stumps = Array.from(stumps);
+    console.log(stumps);
+}
+
+function stumpify(code) {
+    let splitStr = code.split(' ');
+    return splitStr[0] + ' ' + splitStr[1];
 }
